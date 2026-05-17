dc0d6fb9d1b78a71fef0260ef634ca28a5637481
s1rcody69 <cody200600@gmail.com>
2026-05-18 00:45:24 +0300

    Fix: Change root cariable to enable login

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Main entrypoint - run this file to start app
-
 import tkinter as tk
 from database.db_manager import DatabaseManager
 from gui.login_screen import LoginScreen
@@ -8,13 +6,11 @@ from gui.login_screen import LoginScreen
 def main():
     print("Starting SmartFit Gym System...")
 
-    # Step 1: Initialize database and create tables
+    # Initialize database
     db = DatabaseManager()
-
-    # Step 2: Seed default admin and plans (only runs if empty)
     db.seed_default_data()
 
-    # Step 3: Launch the login window
+    # Launch login window
     root = tk.Tk()
     LoginScreen(root)
     root.mainloop()
